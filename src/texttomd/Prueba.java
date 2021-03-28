1fcc77b6e7c69ff4946b1951fe68082e9205e321
jmcastinheira <jmcastinheira@replicant.entelequia.info>
2021-03-28 20:29:06 +0200

    añado expresión regular, y varios strin y machea

--- src/texttomd/Prueba.java
+++ src/texttomd/Prueba.java
@@ -3,8 +3,8 @@ package texttomd;
 public class Prueba {
 
 	public static void main(String[] args) {
-		String regex = "";
-		String[] cadenas = {"1.1.1. Prueba"};
+		String regex = "^[\\d\\.][\\d\\.]*.*";
+		String[] cadenas = {"1.1.1. Prueba", "1.1.2 Moncho me hace trabajar", "1.1 Título segundo"};
 		for (String s: cadenas)
 		System.out.println(s + " "+ s.matches(regex));
 		
