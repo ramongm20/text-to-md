f7de6fcb154f465f793597cecc24145c25197bb8
Ramón García <ramongm20@educastur.es>
2021-03-29 12:24:24 +0200

    Ahora los encabezados se marcan correctamente dependiendo solo de la expresión regular para localizarlos

--- src/texttomd/Documento.java
+++ src/texttomd/Documento.java
@@ -7,26 +7,42 @@ public class Documento extends ArrayList<String> {
 	private static final long serialVersionUID = 1L;
 	
 	public void marcaEncabezados() {
-		String regex = "^(\\d+\\.)+.*";
-		for (int i=0; i<this.size(); i++) {
-			String lnActiva = this.get(i);
-			String lnAnterior = getLn(i-1);
-			String lnSiguiente = getLn(i+1);
-		if (lnActiva.matches(regex) && lnSiguiente != null && !lnSiguiente.matches(regex)) {
+		String regex = "(^[\\d]+)(\\.)([\\d\\.]*)(.*)";
+		boolean[] coincidencias = new boolean[this.size()];
+for (int i=0; i<this.size(); i++) {
+	coincidencias[i] = this.get(i).matches(regex);
+}
+int principio = 0;
+int fin = 0;
+int p = 0;
+int f = 0;
+for (int i=0; i<coincidencias.length; i++) {
+	if(coincidencias[i]) {
+		p = i;
+		while (i < coincidencias.length && coincidencias[i]) {
+			i++;
+			f = i;
+		}
+		principio += p;
+		fin += f;
+		if (fin-principio == 1) {
+			String lnActiva = this.get(principio);
 			StringBuilder marca = new StringBuilder(" ");
+			marca.append(lnActiva);
 			int punto = lnActiva.indexOf('.');
 			do {
 				marca.insert(0, '#');
 				punto = lnActiva.indexOf('.', punto+1);
 			} while (punto>0 && punto != (lnActiva.length()-1));
-			if (lnAnterior != null && !lnAnterior.isBlank())
-				marca.insert(0, '\n');
-			marca.append(lnActiva);
-			if (lnSiguiente != null && !lnSiguiente.isBlank())
-				marca.append('\n');
-			this.set(i, marca.toString());
+			this.set(principio, marca.toString());
 		}
-		}
+		this.add(principio, "ppp");
+		fin++;
+		this.add(fin, "fff");
+		principio = principio + 2 - p;
+		fin = fin + 1 - f;
+	}
+}
 	}
 
 	@Override
@@ -37,12 +53,8 @@ public class Documento extends ArrayList<String> {
 	}
 
 	public boolean coincideÚltimaLn(String ln) {
-		if (this.size()==0) return false;
-		return ln.equals(this.get(this.size()-1)) ? true: false;
+		return this.size()>0 && ln.equals(this.get(this.size()-1)) ? true: false;
 	}
 
-	private String getLn(int i) {
-		return i>0 && i<(this.size()-1) ? this.get(i): null;
-	}
-	
+
 }
